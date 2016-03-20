f0d4c09a0a5513f4af810af08db0190cecba8b46
zhuao <zhuao1051@gmail.com>
2016-03-21 03:29:30 +0800

    Change to chrome driver

--- src/test/java/com/thoughtworks/bdd/steps/Hooks.java
+++ src/test/java/com/thoughtworks/bdd/steps/Hooks.java
@@ -3,7 +3,8 @@ package com.thoughtworks.bdd.steps;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import sun.plugin2.util.SystemUtil;
 
 public class Hooks {
 
@@ -15,8 +16,18 @@ public class Hooks {
 
     @Before
     public void beforeScenario() {
-        driver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver", detectDriverPath());
+        driver = new ChromeDriver();
         getDriver().get("https://github.com");
+    }
+
+    private String detectDriverPath() {
+        int osType = SystemUtil.getOSType();
+        if (osType == SystemUtil.WINDOWS) {
+            return getClass().getClassLoader().getResource("bin/windows/chromedriver.exe").getPath();
+        }
+        return getClass().getClassLoader().getResource("bin/mac/chromedriver").getPath();
+
     }
 
     @After
