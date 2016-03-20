2110a8d1e04bb4c531f7364ef604b27dfbfd2e02
zhuao <zhuao1051@gmail.com>
2016-03-21 03:59:06 +0800

    Change firefox back

--- src/test/java/com/thoughtworks/bdd/steps/Hooks.java
+++ src/test/java/com/thoughtworks/bdd/steps/Hooks.java
@@ -3,8 +3,7 @@ package com.thoughtworks.bdd.steps;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import sun.plugin2.util.SystemUtil;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Hooks {
 
@@ -16,18 +15,8 @@ public class Hooks {
 
     @Before
     public void beforeScenario() {
-        System.setProperty("webdriver.chrome.driver", detectDriverPath());
-        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         getDriver().get("https://github.com");
-    }
-
-    private String detectDriverPath() {
-        int osType = SystemUtil.getOSType();
-        if (osType == SystemUtil.WINDOWS) {
-            return getClass().getClassLoader().getResource("bin/windows/chromedriver.exe").getPath();
-        }
-        return getClass().getClassLoader().getResource("bin/mac/chromedriver").getPath();
-
     }
 
     @After
