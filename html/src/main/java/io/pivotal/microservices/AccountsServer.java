fd459995f313f058d169719879b1280438db9cd2
zhuao <zhuao1051@gmail.com>
2016-09-01 17:48:37 +0800

    Add right service

---
+++ html/src/main/java/io/pivotal/microservices/AccountsServer.java
@@ -0,0 +1,43 @@
+package io.pivotal.microservices;
+
+import io.pivotal.microservices.accounts.AccountRepository;
+import io.pivotal.microservices.accounts.AccountsConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+import java.util.logging.Logger;
+
+/**
+ * Run as a micro-service, registering with the Discovery Server (Eureka).
+ * <p>
+ * Note that the configuration for this application is imported from
+ * {@link AccountsConfiguration}. This is a deliberate separation of concerns.
+ * 
+ * @author Paul Chapman
+ */
+@EnableAutoConfiguration
+//@EnableDiscoveryClient
+@Import(AccountsConfiguration.class)
+public class AccountsServer {
+
+	@Autowired
+	protected AccountRepository accountRepository;
+
+	protected Logger logger = Logger.getLogger(AccountsServer.class.getName());
+
+	/**
+	 * Run the application using Spring Boot and an embedded servlet engine.
+	 * 
+	 * @param args
+	 *            Program arguments - ignored.
+	 */
+	public static void main(String[] args) {
+		// Tell server to look for accounts-server.properties or
+		// accounts-server.yml
+		System.setProperty("spring.config.name", "accounts-server");
+
+		SpringApplication.run(AccountsServer.class, args);
+	}
+}
